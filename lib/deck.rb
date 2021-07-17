e7babd3b00e8ab94ad3123d30279e98d72fcaf43
Rene Casco <rene.casco@icloud.com>
2021-07-16 18:29:15 -0600

    initializes deck with 52 cards and adds attr_reader

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,12 @@
+require './lib/card'
+
 class Deck
+  attr_reader :cards
+
+  def initialize
+    ranks = %w[ace 2 3 4 5 6 7 8 9 10 jack queen king]
+    suits = %w[hearts spades diamonds clubs]
+    @cards = ranks.flat_map { |rank| suits.map { |suit| Card.new(rank, suit) } }
+  end
 
 end
