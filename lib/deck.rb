3b8d8eb5d3b93e1dc6a62e8e26a8e43ab5cff876
Rene Casco <rene.casco@icloud.com>
2021-07-16 19:46:26 -0600

    refactor initialize and adds method generate_cards

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,9 +4,15 @@ class Deck
   attr_reader :cards
 
   def initialize
+    @cards = generate_cards
+  end
+
+  private
+
+  def generate_cards
     ranks = %w[ace 2 3 4 5 6 7 8 9 10 jack queen king]
     suits = %w[hearts spades diamonds clubs]
-    @cards = ranks.flat_map { |rank| suits.map { |suit| Card.new(rank, suit) } }
+    ranks.flat_map { |rank| suits.map { |suit| Card.new(rank, suit) } }
   end
 
 end
