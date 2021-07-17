3c169b2a1a9e5e1dbd5d1e44a70e3d5e299d0761
Rene Casco <rene.casco@icloud.com>
2021-07-16 20:45:42 -0600

    refactors generate_cards method so cards in the array are strings

--- lib/deck.rb
+++ lib/deck.rb
@@ -12,7 +12,6 @@ class Deck
   def generate_cards
     ranks = %w[ace 2 3 4 5 6 7 8 9 10 jack queen king]
     suites = %w[hearts spades diamonds clubs]
-    ranks.flat_map { |rank| suites.map { |suite| Card.new(rank, suite) } }
+    ranks.flat_map { |rank| suites.map { |suite| Card.new(rank, suite).to_text } }
   end
-
 end
